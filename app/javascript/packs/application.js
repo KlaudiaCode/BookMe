98967dfd5b80470ce67de45d94df146c951549e2
klaudiacode <oposyyy@gmail.com>
2021-11-28 13:46:28 +0100

    Repair toggle menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import "bootstrap"
+import "bootstrap-sprockets"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
