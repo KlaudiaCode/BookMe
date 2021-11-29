8491e7caad1b9cde3315e5dbe50fa84f748bf01d
klaudiacode <oposyyy@gmail.com>
2021-11-29 20:28:47 +0100

    Add sending trades

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,9 +5,25 @@ class TradesController < ApplicationController
   def new
     @item = Item.find(params[:item_id])
     @my_items = current_user.items
+    @trade = Trade.new
   end
 
   def create
+    @trade = Trade.new(trade_params)
+    @trade.trader_id = current_user.id
+    if @trade.save
+      flash[:success] = 'Oferta została wysłana do właściciela.'
+      redirect_to items_path
+    else 
+      flash[:warning] = @trade.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def trade_params
+    params.require(:trade).permit(:owner_id, :owner_item_id, :trader_item_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :items
-  resources :users
+  resources :users, except: :index
   resources :trades, only: [:new, :create]
 end
