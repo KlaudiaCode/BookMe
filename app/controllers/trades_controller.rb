b776c8c052bd17adc3cfd42dc52eda55457ff36c
klaudiacode <oposyyy@gmail.com>
2021-11-29 19:28:13 +0100

    Add trading page

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,9 +1,13 @@
 class TradesController < ApplicationController
 
+  before_action :require_user
+
   def new
+    @item = Item.find(params[:item_id])
+    @my_items = current_user.items
   end
 
   def create
   end
-  
+
 end
