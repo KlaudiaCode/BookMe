46d4f31f330947b9fc1035848900eb9eacef3268
klaudiacode <oposyyy@gmail.com>
2021-12-12 19:21:27 +0100

    Added gem annotate Added validations to models

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: genres
+#
+#  id         :integer          not null, primary key
+#  group      :boolean
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Genre < ApplicationRecord
   has_many :item_genres
   has_many :items, through: :item_genres
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  is_admin        :boolean
+#  name            :string           not null
+#  password_digest :string
+#  phone           :integer
+#  surname         :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   has_many :items
   has_one :address
@@ -5,4 +20,5 @@ class User < ApplicationRecord
   has_many :trades
 
   validates :name, :surname, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
