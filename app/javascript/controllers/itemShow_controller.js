607ad1e0e48432f51bdc4450fdb43a25fd833385
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-12-14 13:04:16 +0100

    Changed static map to dynamic on item show Added itemShow js controller

---
+++ app/javascript/controllers/itemShow_controller.js
@@ -0,0 +1,47 @@
+import { Controller} from "stimulus"
+
+export default class extends Controller {
+  static targets = ["location", "map", "latitude", "longitude"]
+
+  connect() {
+    if (typeof (google) != "undefined"){
+      this.initializeMap()
+    }
+  }
+
+  initializeMap() {
+    this.map()
+    this.marker()
+  }
+
+  map() {
+    if(this._map == undefined) {
+      this._map = new google.maps.Map(this.mapTarget, {
+        center: new google.maps.LatLng(
+          this.latitudeTarget.value,
+          this.longitudeTarget.value
+        ),
+        zoom: 13,
+        clickableIcons: false,
+        mapTypeControl: false
+      })
+    }
+    return this._map
+  }
+
+  marker() {
+    if (this._marker == undefined) {
+      this._marker = new google.maps.Marker({
+        map: this.map(),
+        anchorPoint: new google.maps.Point(0,0)
+      })
+      let mapLocation = {
+        lat: parseFloat(this.latitudeTarget.value),
+        lng: parseFloat(this.longitudeTarget.value)
+      }
+      this._marker.setPosition(mapLocation)
+      this._marker.setVisible(true)
+    }
+    return this._marker
+  }
+}
